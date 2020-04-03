a7614abbec21c46a1f5aac61dd5264ce3969a708
ChocolateCode4 <azulinrose@gmail.com>
2020-04-03 21:11:09 +0800

    Proper Control System Idea

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
-/* To Do:
-MAIN: fucking jump up needs to be done using limiter for playerpos.y and then apply it also to all objects
+  /* To Do:
+MAIN: perform attack keeps being called, instead use gameLoop and inherent class variables
 2) MAIN: sprtites/json data files 
 then combat mechanics (damage, basic attacks)
 3) create atlas sprites, use our JSON expertise.
@@ -28,7 +28,13 @@ Loader.add([
   "assets/forTest/blue.png"
   
   ]).load(window.onload = () => orientation());
-  
+
+async function fetchData(data) {
+  let newData = await fetch(data);
+  let dataObj = await newData.json();
+  return dataObj;
+}
+
 class Ground {
   constructor(x,y,w,h,col) {
     this.object = new Graphics();
@@ -40,6 +46,33 @@ class Ground {
     this.object.x = x;
     this.propertyPointer = this.object;
     app.stage.addChild(this.object);
+  }
+}
+
+class OnScreenControl {
+  constructor(x,y,entity) {
+    this.x = x;
+    this.y = y;
+    this.size = 30;
+    this.button2 = new Graphics();
+    this.button2.beginFill("0xffffff", 0.75)
+    this.button2.drawCircle(this.x, this.y, this.size);
+    this.button2.endFill();
+    this.button1 = new Graphics();
+    this.button1.beginFill("0xffffff", 0.75)
+    this.button1.drawCircle(this.x-(this.size*2.25), this.y + (this.size/4), this.size);
+    this.button1.endFill();
+    this.button1.interactive = true;
+    this.button2.interactive = true;
+   app.stage.addChild(this.button1, this.button2);
+  }
+  defaultListener(entity) {
+    this.button1.touchstart = event => {
+      moveToContainer("lp", entity);
+    }
+    this.button2.touchstart = event => {
+      moveToContainer("sp", entity);
+    }
   }
 }
 
@@ -108,16 +141,22 @@ class GameRules {
 }
 
 class Player {
-  constructor(sprite, health, selected, side) {
+  constructor(sprite, health, selected, side, jsonFile) {
+    this.jsonFile = jsonFile;
+    this.newData;
+    this.dataObj;
     this.sprite = new Sprite(Resources[sprite].texture);
     this.selected = selected;
     this.side = side;
     this.standingPoint = 0;
+    this.front; //sends left or right data
     this.state = "idle";
+    this.enemy;
     this.outofbounds;
     this.canJump = true;
     this.canWalk = true;
     this.canAttack = true;
+    this.canBeHit = true;
     this.health = health;
     this.propertyPointer = this.sprite;
     this.posPointer = this.sprite.position;
@@ -148,16 +187,84 @@ class Player {
       this.spawn.x = 100;
       this.spawn.y = 200;
     } else if(this.side === "p2") {
-      this.spawn.x = 500;
+      this.spawn.x = 330;
       this.spawn.y = 200;
     }
     app.stage.addChild(this.sprite);
     this.sprite.position.x = this.spawn.x;
     this.sprite.position.y = this.spawn.y;
   }
+  
+ async dataFetch() {
+    this.newData = await fetch(this.jsonFile);
+    this.dataObj = await this.newData.json()
+  }
+  
+  listenAttackInput() {
+    let movesQueue = new Array;
+    let listen = () => {
+      let lastInput = this.moveContainer[this.moveContainer.length-1];
+     /* movesQueue.forEach(eQueue => {
+        let elimInput = eQueue.inputR.filter(eQueried => {
+          return eQueried !== lastInput;
+        });
+      });*/
+      //showConsole(lastInput)
+      for(let x =0; x<movesQueue.length;x++) {
+        //gets all inputR but still an array of inputs
+        let allInputs = movesQueue[x].inputR;
+        
+        for(let y=0;y<this.moveContainer.length;y++) {
+          if(allInputs[0] == this.moveContainer[y] && allInputs[1] == this.moveContainer[y+1]&& allInputs[2] == this.moveContainer[y+2]&& allInputs[3] == this.moveContainer[y+3]&& allInputs[4] == this.moveContainer[y+4]&& allInputs[5] == this.moveContainer[y+5]) {
+            this.performAttack(movesQueue[x]);
+          }
+         
+        }
+        /*
+        if(lastInput == allInputs[0]) {
+           
+        } else if(lastInput !== allInputs[0]) {
+          //nest a ghost input check
+          allInputs.splice(0,1);
+          resetInput();
+        }
+        if(allInputs.length <= 0) {
+          showConsole("performing: " + movesQueue[x].name)
+        }*/
+        //showConsole(allInputs);
+        /*
+        if(allInputs[x].length == 0) {
+          performAttack(allInputs[x].name);
+        }
+        if(lastInput == ) {
+          showConsole("down")
+        }*/
+      }
+    }
+    if(this.dataObj !== undefined) {
+     this.dataObj.moves.forEach(eMoves => {
+       movesQueue.push(eMoves);
+     });
+     listen()
+    }
+  }
+  performAttack(attack) {
+    showConsole(attack.name);
+    console.log(attack.property)
+    switch(attack.property) {
+      case "projectile":
+        console.log("hadu fucking ken");
+        break;
+      default:
+        return 0
+    }
+  }
 }
 
 class Movement {
+  static crouch(dir,entity) {
+    
+  }
   static walk(dir,ent) {
     if(ent.canWalk == true) {
       switch(dir) {
@@ -211,7 +318,8 @@ let player1,
     player2,
     playerGroup = [],
     moveStorage = [],
-    touchField = new Graphics();
+    touchField = new Graphics(),
+    moveTimer = new Ticker();
     
 newConsole = new Text("Console: ", {color: 0xffffff});
 newConsole.x = 10;
@@ -227,8 +335,12 @@ function init() {
   //create the canvas
   document.body.appendChild(app.view);
   //define players instance
-  player1 = new Player("assets/forTest/blue.png", 100, "blue", "p1");
+  player1 = new Player("assets/forTest/blue.png", 100, "blue", "p1", "scripts/characters/joe.json");
+  player1.dataFetch();
+ // player1.listenAttackInput();
   player2 = new Player("assets/forTest/blue.png", 100, "blue2", "p2");
+  player1.enemy = player2;
+  player2.enemy = player1;
   playerGroup.push(player1, player2);
   GameRules.rigidBodies.push(player1,player2);
 
@@ -237,9 +349,21 @@ function init() {
   GameRules.groundObjects.push(ground);
  // control
  touchControl(player1);
+ let touchButtons = new OnScreenControl(100,200, player1)
+ touchButtons.defaultListener(player1)
  // render
  app.ticker.add(delta => gameLoop(delta))
  app.renderer.render(app.stage);
+}
+
+function updateFront(arr) {
+  arr.forEach(entity => {
+    if(entity.posPointer.x > entity.enemy.posPointer.x) {
+      entity.front = "left";
+    } else if(entity.posPointer.x < entity.enemy.posPointer.x) {
+      entity.front = "right";
+    }
+  });
 }
 
 function showConsole(text) {
@@ -251,16 +375,17 @@ function moveToContainer(move, entity) {
   function reset() {
     entity.moveContainer = [];
   }
-  if(entity.moveContainer.length > 4) {
+  if(entity.moveContainer.length > 100) {
     reset();
   }
   function filterDupl(moves) {
     for(x=0;x<moves.length;x++) {
-      if(moves[x] === moves[x+1]) {
+      if(moves[x] === moves[x+1] && moves[x+1] !== "lp") {
         moves.pop();
       }
     }
   }
+  
   entity.moveContainer.push(move);
   filterDupl(entity.moveContainer)
 }
@@ -271,7 +396,7 @@ function touchControl(entity) {
     let touch = event.data.global;
     difference = touch.x - touchStorage.x;
     differenceUp = touch.y - touchStorage.y;
-    if (differenceUp <= -30 && difference >= diff + 30) {
+    if (differenceUp <= -20 && difference >= diff + 30) {
       //jump forward
       moveToContainer("ur", entity);
       Movement.jump("upright", entity);
@@ -279,15 +404,16 @@ function touchControl(entity) {
           //crouch guard
           moveToContainer("dl", entity);
           Movement.crouch("downleft", entity);
-    } else if (differenceUp <= -30 && difference <= diff - 20) {
+    } else if (differenceUp <= -20 && difference <= diff - 20) {
           //jump left
       moveToContainer("ul", entity);
       Movement.jump("upleft", entity);
     }
-    if(differenceUp <= -50 && difference <= 2) {
+    if(differenceUp <= -20 && difference <= 2) {
+      moveToContainer("u",entity);
       Movement.jump("up", entity)
     }
-    if(differenceUp >= 50 && difference ) {
+    if(differenceUp >= 10 && difference <= 4 ) {
       moveToContainer("d", entity);
     }
     // config for left-right movement
@@ -318,11 +444,13 @@ function touchControl(entity) {
   };
   touchField.touchend = (event => Movement.walk("neutral", entity));
 } 
-
+moveTimer.update()
 function gameLoop(delta) {
   GameRules.gravity("on");
   GameRules.playerMove();
+  player1.listenAttackInput();
   
+  updateFront([player1,player2]);
 }
 
 function orientation() {
